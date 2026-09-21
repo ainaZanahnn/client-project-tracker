aac67b447c3d00c41ac302ef08fbd3824557d07f
escapeescape <151479440+ainaZanahnn@users.noreply.github.com>
2026-09-22 03:21:46 +0800

    update app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import "./App.css";
+
 import ProjectsPage from "./pages/projectPage";
+import ProjectDetailPage from "./pages/ProjectDetailPage";
 
 function App() {
-  return <ProjectsPage />;
+  const [selectedProjectId, setSelectedProjectId] = useState<number | null>(null);
+
+  if (selectedProjectId !== null) {
+    return (
+      <ProjectDetailPage
+        projectId={selectedProjectId}
+      />
+    );
+  }
+
+  return (
+    <ProjectsPage onProjectClick={setSelectedProjectId}/>
+  );
 }
 
 export default App;
