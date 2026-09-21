f4f78e44b0d5a836b2174f389acb9b839b05a924
escapeescape <151479440+ainaZanahnn@users.noreply.github.com>
2026-09-21 23:22:52 +0800

    add route for listing task & mark complete and fix validateId

--- backend/src/routes/projects.ts
+++ backend/src/routes/projects.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import { getProjects, createProject, updateProject, deleteProject } from "../controllers/projects";
-import { validateProject, validateId } from "../middleware/validation";
+import { validateProject, validateId, validateTask } from "../middleware/validation";
+import { getTasksByProject, markTaskComplete } from "../controllers/tasks";
 
 const router = Router();
 
+//route for project
 router.get("/", getProjects);
 router.post("/", validateProject, createProject);
-router.put("/:id", validateId, validateProject, updateProject);
-router.delete("/:id", validateId, deleteProject);
+router.put("/:id", validateId("id"), validateProject, updateProject);
+router.delete("/:id", validateId("id"), deleteProject);
+
+//route for tasks
+router.get("/:projectId/tasks",validateId("projectId"), validateTask, getTasksByProject);
+
+router.patch("/:projectId/tasks/:taskId/complete", validateId("projectId"), validateId("taskId"), markTaskComplete);
 
 export default router;
