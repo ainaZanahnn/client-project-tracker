d3163a3d694ecc5d7878ca66a37a0bda48588aa6
escapeescape <151479440+ainaZanahnn@users.noreply.github.com>
2026-09-22 07:45:15 +0800

    fix App.tsx fromatting

--- frontend/src/services/projectApi.ts
+++ frontend/src/services/projectApi.ts
@@ -3,11 +3,10 @@ import type { Project } from "../types/project";
 const API_URL = "http://localhost:5000/api";
 
 export async function getProjects(): Promise<Project[]> {
-  const response = await fetch(`${API_URL}/projects`);
+    const response = await fetch(`${API_URL}/projects`);
 
-  if (!response.ok) {
-    throw new Error("Failed to fetch projects");
-  }
-
-  return response.json();
+    if (!response.ok) {
+        throw new Error("Failed to fetch projects");
+    }
+    return response.json();
 }
